26d7f17242e933791efc3184d8f3f8099e51b021
CERSGIS iMac <geraldkugblenu52@gmail.com>
2024-03-04 12:45:15 +0000

    models

--- Aboutus/models.py
+++ Aboutus/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class AboutUs(models.Model):
+ title = models.CharField(max_length=200)
+ our_mission = models.TextField()
+ our_vision = models.TextField()
+ why_us = models.TextField()
+ meet_our_team = models.TextField()
--- Home/urls.py
+++ Home/urls.py
@@ -5,5 +5,5 @@ app_name = "Homes"
 urlpatterns = [
     path("", views.Home, name="Home"),
         path('contact-us/', views.Home, name='contact_us'),
-    path("about-us/", views.Home, name="about_us"),
+    path('about-us/', views.Home, name='about_us'),
 ]
