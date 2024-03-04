bc6bd93636fbe1600ceae9c17720fc0989ea633d
CERSGIS iMac <geraldkugblenu52@gmail.com>
2024-03-04 15:10:32 +0000

    modelsupers

--- Aboutus/models.py
+++ Aboutus/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 class AboutUs(models.Model):
  title = models.CharField(max_length=200)
+ description = models.TextField(null=True,blank=True)
  our_mission = models.TextField()
  our_vision = models.TextField()
  why_us = models.TextField()
  meet_our_team = models.TextField()
+ 
+ def __str__(self):
+        return self.title
